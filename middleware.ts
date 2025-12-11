ed8e16d83331c4428fe54cd0698151e857608b27
Ishan Misra <ishanm1603@gmail.com>
2025-12-11 17:55:18 +0530

    Fix routing loop

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,7 @@ import {
   apiAuthPrefix,
   publicRoutes,
   authRoutes,
-} from "./routes";
-
+} from "@/routes";
 import authConfig from "./auth.config";
 
 const { auth } = NextAuth(authConfig);
@@ -14,11 +13,18 @@ const { auth } = NextAuth(authConfig);
 export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
+
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
   if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
@@ -28,9 +34,11 @@ export default auth((req) => {
   if (!isLoggedIn && !isPublicRoute) {
     return Response.redirect(new URL("/auth/sign-in", nextUrl));
   }
+
   return null;
 });
 
 export const config = {
+  // copied from clerk
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
