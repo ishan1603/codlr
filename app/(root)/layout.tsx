e7aab82ef7e9310a4874ec3447c5ab262860526a
Ishan Misra <ishanm1603@gmail.com>
2025-12-16 01:55:41 +0530

    Home Page

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "Codlr - Editor",
+    default: "Code Editor ",
+  },
+};
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {/* Header */}
+      {/* Backgroound effects and grid */}
+      {/* main */}
+      {/* footer */}
+    </>
+  );
+}
